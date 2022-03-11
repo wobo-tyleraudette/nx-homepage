8f3510630872cdce4d370202d389d483ecbed1ca
Tyler Audette <tyler.audette@workboard.com>
2022-03-11 09:56:52 -0700

    rename to api

--- apps/homepage-api/jest.config.js
+++ apps/homepage-api/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'homepage-bff',
+  displayName: 'homepage-api',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/homepage-bff',
+  coverageDirectory: '../../coverage/apps/homepage-api',
 };
